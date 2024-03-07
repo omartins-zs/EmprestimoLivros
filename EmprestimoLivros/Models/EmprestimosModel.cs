a14c1d81938e8ef5c015cba1de78b3aac089c03f
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-07 07:38:26 -0300

    Criando Emprestimo Model

---
+++ EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado {  get; set; }
+
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+    }
+}
