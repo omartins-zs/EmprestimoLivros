c2e714fa61aede8262a4df7ab1bcfb9ea9a65276
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-08 09:42:06 -0300

    Fazendo efeito e tempo para Exibição da TempData

--- EmprestimoLivros/wwwroot/js/site.js
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -29,4 +29,11 @@ $(document).ready(function () {
             }
         }
     });
+
+    setTimeout(function () {
+        $(".alert").fadeOut("slow", function () {
+            $(this).alert('close');
+        });
+    }, 5000)
+
 })
